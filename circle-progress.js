19ca28f15bfa056d15c65edeaa19db498e1c2056
ahmedalmaster <gsospambox2016@gmail.com>
2016-07-14 17:43:09 +0200

    Update circle-progress.js

--- circle-progress.js
+++ circle-progress.js
@@ -1,80 +1,125 @@
-(function($){
-	$.fn.circleGraphic=function(options){
-		$.fn.circleGraphic.defaults={
-			color:'#F90',
-			startAngle: 0,
-			//endAngle:50
-		};
+/**************************************************************
+*
+* Progress Circle 1.0
+*
+**************************************************************/
 
-		var opts = $.extend({},$.fn.circleGraphic.defaults,options);
-		
-		var percentage=this.html();
-		var ID="c"+percentage+Math.random();
-		//alert(ID);
+( function( $ ){
+	var ProgressCircle = function( element, options ){
 
-		this.append("<canvas id='"+ID+"'></canvas>");
+		var settings          = $.extend( {}, $.fn.progressCircle.defaults, options );
+		var thicknessConstant = 0.02;
+		var nRadian           = 0;
 
-		var canvas=document.getElementById(ID),
-			context=canvas.getContext('2d');
+		computePercent();
+		setThickness();
 
-		var Width = this.width();
-		this.height(Width);
-		var Height = this.height();
+		var border      = ( settings.thickness * thicknessConstant ) + 'em';
+		var offset      = ( 1 - thicknessConstant * settings.thickness * 2 ) + 'em';
+		var circle      = $( element );
+		var progCirc    = circle.find( '.prog-circle' );
+		var circleDiv   = progCirc.find( '.bar' );
+		var circleSpan  = progCirc.children( '.percenttext' );
+		var circleFill  = progCirc.find( '.fill' );
+		var circleSlice = progCirc.find( '.slice' );
 
-		canvas.width = Width;
-		canvas.height = Height;
+		if ( settings.nPercent == 0 ) {
+			circleSlice.hide();
+		} else {
+			resetCircle();
+			transformCircle( nRadians, circleDiv );
+		}
+		setBorderThickness();
+		updatePercentage();
+		setCircleSize();
 
-		var startAngle = opts.startAngle,
-			endAngle = percentage/100,
-			angle = startAngle,
-			radius = Width*0.4;
-
-		function drawTrackArc(){
-			context.beginPath();
-			context.strokeStyle = '#ECECEC';
-			context.lineWidth = 5;
-			context.arc(Width/2,Height/2,radius,(Math.PI/180)*(startAngle*360-90),(Math.PI/180)*(endAngle*360+270),false);
-			context.stroke();
-			context.closePath();
+		function computePercent () {
+			settings.nPercent > 100 || settings.nPercent < 0 ? settings.nPercent = 0 : settings.nPercent;
+			nRadians = ( 360 * settings.nPercent ) / 100;
 		}
 
-		function drawOuterArc(_angle,_color){
-			var angle = _angle;
-			var color = _color;
-			context.beginPath();
-			context.strokeStyle = color;
-			context.lineWidth = 10;
-			context.arc(Width/2,Height/2,radius,(Math.PI / 180) * (startAngle * 360 - 90), (Math.PI / 180) * (angle * 360 - 90), false);
-	       	context.stroke();
-	       	context.closePath();
-		}	
-
-		function numOfPercentage(_angle,_color){
-			var angle = Math.floor(_angle*100)+1;
-			var color=_color;
-			context.font = "50px fantasy";
-			context.fillStyle = color;
-			var metrics = context.measureText(angle);
-			var textWidth = metrics.width;
-			var xPos = Width/2-textWidth/2,
-				yPos = Height/2+textWidth/2;
-			context.fillText(angle+"%",xPos,yPos);
+		function setThickness () {
+			if ( settings.thickness > 10 ) {
+				settings.thickness = 10;
+			} else if ( settings.thickness < 1 ) {
+				settings.thickness = 1;
+			} else {
+				settings.thickness = Math.round( settings.thickness );
+			}
 		}
 
-		function draw(){
-			var loop = setInterval(function(){
-				context.clearRect(0,0,Width,Height);
-				drawTrackArc();
-				drawOuterArc(angle,opts.color);
-				numOfPercentage(angle,opts.color);
-				angle+=0.01;
-				if(angle>endAngle){
-					clearInterval(loop);
-				}
-
-			},1000/60);
+		function setCircleSize ( ) {
+			progCirc.css( 'font-size', settings.circleSize + 'px' );
 		}
-		draw();
-		return this;
+
+		function transformCircle ( nRadians, cDiv ) {
+			var rotate = "rotate(" + nRadians + "deg)";
+	    cDiv.css({
+	      "-webkit-transform" : rotate,
+	      "-moz-transform"    : rotate,
+	      "-ms-transform"     : rotate,
+	      "-o-transform"      : rotate,
+	      "transform"         : rotate
+	    });
+	    if( nRadians > 180 ) {
+		    transformCircle( 180, circleFill );
+		    circleSlice.addClass( ' clipauto ');
+			}
+		}
+
+		function setBorderThickness () {
+			progCirc.find(' .slice > div ').css({
+				'border-width' : border,
+				'width'        : offset,
+				'height'       : offset
+			})
+			progCirc.find('.after').css({
+				'top'    : border,
+				'left'   : border,
+				'width'  : offset,
+				'height' : offset
+			})
+		}
+
+		function resetCircle () {
+			circleSlice.show();
+			circleSpan.text( '' );
+			circleSlice.removeClass( 'clipauto' )
+			transformCircle( 20, circleDiv );
+			transformCircle( 20, circleFill );
+			return this;
+		}
+
+		function updatePercentage () {
+			settings.showPercentText && circleSpan.text( settings.nPercent + '%' );
+		}
 	};
-})(jQuery);
+
+	$.fn.progressCircle = function( options ) {
+		return this.each( function( key, value ){
+      var element = $( this );
+      if ( element.data( 'progressCircle' ) ) {
+      	var progressCircle = new ProgressCircle( this, options );
+      	return element.data( 'progressCircle' );
+      }
+    	$( this ).append( '<div class="prog-circle">' +
+	                 			'	<div class="percenttext"> </div>' +
+	                 			'	<div class="slice">' +
+	                 			'		<div class="bar"> </div>' +
+	                 			'		<div class="fill"> </div>' +
+	                 			'	</div>' +
+	                 			'	<div class="after"> </div>' +
+	                 			'</div>');
+      var progressCircle = new ProgressCircle( this, options );
+      element.data( 'progressCircle', progressCircle );
+  	});
+	};
+
+	$.fn.progressCircle.defaults = {
+		nPercent        : 50,
+		showPercentText : true,
+		circleSize      : 100,
+		thickness       : 3
+	};
+
+})( jQuery );
